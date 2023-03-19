01e7309ffbfc56bc5af3613f0679486e05df394a
ericadu <erica.c.du@gmail.com>
2023-03-19 19:41:35 -0400

    Working bread bot.

---
+++ api/app.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import replicate
+from PIL import Image
+import os
+
+app = Flask(__name__)
+
+REPLICATE_API = "47dcfd8c74eeb1d952d40efa0754f8f689acf3a8"
+
+def compress(image_file):
+    filepath = os.path.join(os.getcwd(), image_file)
+
+    image = Image.open(filepath)
+    basewidth = 550
+    wpercent = (basewidth/float(image.size[0]))
+    hsize = int((float(image.size[1])*float(wpercent)))
+    
+    image = image.resize((basewidth,hsize), Image.Resampling.LANCZOS)
+    image = image.rotate(270)
+    image.save(image_file, 
+                 "JPEG", 
+                 optimize = True, 
+                 quality = 50)
+    return
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    if 'file' not in request.files:
+        return 'No file part in the request', 400
+    
+    file = request.files['file']
+    # print(file.stream.read())
+    if file.filename == '':
+        return 'No selected file', 400
+
+    filename = '/tmp/' + file.filename;
+    file.save(filename)
+    
+    compress(filename)
+    print(file.content_length)
+    
+    model = replicate.models.get("timothybrooks/instruct-pix2pix")
+    version = model.versions.get("30c1d0b916a6f8efce20493f5d61ee27491ab2a60437c13c588468b9810ec23f")
+    
+    inputs = {
+        'prompt': 'add bread to the fridge',
+        'image': open(filename, 'rb'),
+        'num_outputs': 1,
+        'num_inference_steps': 50,
+        'guidance_scale': 10.47,
+        'image_guidance_scale': 1.5,
+        'schedule': 'DPMSolverMultistep'
+        
+    }
+    output = version.predict(**inputs)
+    print(output)
+    if len(output) < 1:
+        return 'No output', 400
+    print(output[0])
+    return jsonify({
+        'image_url': output[0]
+    }), 200
+
+if __name__ == '__main__':
+    app.run()
--- app/lib/image_from_gallery.dart
+++ app/lib/image_from_gallery.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
-
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:get_bready/fixed_height_button.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:convert';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
 
 class ImageFromGallery extends StatefulWidget {
   const ImageFromGallery({
@@ -16,11 +21,61 @@ class ImageFromGallery extends StatefulWidget {
 class _ImageFromGalleryState extends State<ImageFromGallery> {
   File? _image;
   late ImagePicker imagePicker;
+  String? imgUrl;
 
   @override
   void initState() {
     super.initState();
     imagePicker = ImagePicker();
+  }
+
+  String getNewFilepath(String filepath) {
+    List<String> parts = filepath.split(".");
+    return parts[0] + "_compressed." + parts[1];
+  }
+
+  Future<File> compressAndGetFile(File file, String targetPath) async {
+    var result = await FlutterImageCompress.compressAndGetFile(
+        file.absolute.path, targetPath,
+        minWidth: 550, quality: 50, format: CompressFormat.jpeg);
+    return result!;
+  }
+
+  void sendImage() async {
+    // Replace the URL with the endpoint that you want to send the image to
+    String url = 'https://7cbf-173-56-66-147.ngrok.io/generate';
+
+    // Replace the file path with the path to your image file
+    // File imageFile = _image!;
+    // String newFilepath = getNewFilepath(_image!.absolute.path);
+    File imageFile = _image!;
+
+    // Create a multipart request using the http package
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+
+    var stream = http.ByteStream(DelegatingStream(imageFile.openRead()));
+    // Add the image file to the request
+    http.MultipartFile multipartFile = http.MultipartFile(
+        'file', stream, await imageFile.length(),
+        filename: basename(imageFile.path));
+
+    print(multipartFile.length);
+
+    request.files.add(multipartFile);
+    // request.files
+    //     .add(await http.MultipartFile.fromPath('image', imageFile.path));
+
+    // Send the request and wait for the response
+    http.StreamedResponse response = await request.send();
+
+    response.stream.transform(utf8.decoder).listen((value) {
+      Map<String, dynamic> map = json.decode(value.toString());
+
+      setState(() {
+        imgUrl = map['image_url'];
+      });
+    });
+    // Print the response status cod
   }
 
   @override
@@ -38,43 +93,60 @@ class _ImageFromGalleryState extends State<ImageFromGallery> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                GestureDetector(
-                    onTap: () async {
-                      XFile? image = await imagePicker.pickImage(
-                          source: ImageSource.camera,
-                          imageQuality: 50,
-                          preferredCameraDevice: CameraDevice.rear);
-                      if (image != null) {
-                        setState(() {
-                          _image = File(image.path);
-                        });
-                      }
-                    },
-                    child: Container(
-                      child: _image != null
-                          ? Image.file(
-                              _image!,
-                              width: width,
-                              height: height,
-                              fit: BoxFit.fitHeight,
-                            )
-                          : Container(
-                              decoration:
-                                  const BoxDecoration(color: Colors.amber),
-                              width: width,
-                              height: height,
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.grey[800],
-                              ),
-                            ),
-                    )),
-                if (_image != null)
+                imgUrl != null
+                    ? Container(
+                        height: height,
+                        width: width,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                alignment: Alignment.topLeft,
+                                image: NetworkImage(imgUrl!),
+                                fit: BoxFit.contain)),
+                      )
+                    : GestureDetector(
+                        onTap: () async {
+                          XFile? image = await imagePicker.pickImage(
+                              source: ImageSource.camera,
+                              imageQuality: 50,
+                              preferredCameraDevice: CameraDevice.rear);
+                          if (image != null) {
+                            setState(() {
+                              _image = File(image.path);
+                            });
+                          }
+                        },
+                        child: Container(
+                          child: _image != null
+                              ? Image.file(
+                                  _image!,
+                                  width: width,
+                                  height: height,
+                                  fit: BoxFit.fitHeight,
+                                )
+                              : Container(
+                                  decoration:
+                                      const BoxDecoration(color: Colors.amber),
+                                  width: width,
+                                  height: height,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                        )),
+                if (_image != null || imgUrl != null)
                   FixedHeightRoundedWideButton(
-                    text: 'Use this image',
-                    onPressed: () {
-                      print('hello');
-                    },
+                    text: imgUrl != null ? 'Start Over' : 'Use this image',
+                    onPressed: imgUrl != null
+                        ? () {
+                            setState(() {
+                              _image = null;
+                              imgUrl = null;
+                            });
+                          }
+                        : () {
+                            sendImage();
+                          },
                   )
               ],
             )));
